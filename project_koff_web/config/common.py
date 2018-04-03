12488a70eb2b17100510a3907d1730f77e8028f7
dinoilic <dino.ilic@gmail.com>
2018-04-03 18:51:24 +0200

    Changed common.py for my db config

--- project_koff_web/config/common.py
+++ project_koff_web/config/common.py
@@ -53,7 +53,7 @@ class Common(Configuration):
     # Postgres
     DATABASES = {
         'default': dj_database_url.config(
-            default='postgres://postgres:@postgres:5432/postgres',
+            default='postgres://dilic:@localhost:5432/project_koff_web',
             conn_max_age=int(os.getenv('POSTGRES_CONN_MAX_AGE', 600))
         )
     }
